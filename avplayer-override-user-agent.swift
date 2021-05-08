26d4c5884ca7d368ba7474e618c8aeb2c05fc586
John Spurlock <john.spurlock@gmail.com>
2021-05-07 19:55:40 -0500

    Add examples using the undocumented header approach, and with and without range requests.

--- avplayer-override-user-agent.swift
+++ avplayer-override-user-agent.swift
@@ -2,32 +2,96 @@ import SwiftUI
 import PlaygroundSupport
 import AVKit
 
-let audioUrl = "https://api.livewire.io/reflections/68b6d33a70c0473aac6b00573449b2a9.mp3"
+// Demonstrates the difficulty of setting a custom User-Agent when using Apple's AVPlayer
+
+// These examples all start playing an mp3 at a given url without pre-downloading
+// Similar to Apple Podcast's behavior when hitting the play icon on /undownloaded/ episodes
+
+// This url supports HTTP Range requests
+let audioUrl = "https://api.livewire.io/reflections/68b6d33a70c0473aac6b00573449b2a9.mp3?noip"
+
+// This url does not support HTTP Range requests
+let audioUrlNotSupportingRangeRequests = "https://api.livewire.io/reflections/68b6d33a70c0473aac6b00573449b2a9.mp3?noip&norange"
+
+// Custom User-Agent we would like to send (for server-side attribution purposes)
+let myUserAgent = "MyPodcastApp/1.5 iOS https://mypodcastapp.example.com/"
+
+// standard player used in all examples
 let player = AVPlayer()
-let customLoaderDelegate = CustomLoaderDelegate(userAgent: "MyPodcastApp/1.5 iOS https://mypodcastapp.example.com/")
+
+let customLoaderDelegate = CustomLoaderDelegate(userAgent: myUserAgent)
+
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            // Example using the most basic invocation of playing audio, no user-agent specified
-            Button("Default AV Player") {
+        
+        VStack {
+        
+            // Example 1: Basic AVKit playback, no user-agent specified
+            // Apple's CFNetwork will be used under the hood to make the underlying HTTP calls using its own User-Agent header
+            Button("AVPlayer · default User-Agent ❌") {
                 player.replaceCurrentItem(with: AVPlayerItem(url: URL(string: audioUrl)!))
                 player.play() // "Apple Core Media"
             }.padding()
        
-            // Example using a custom resource loader, can set the user-agent
-            Button("Custom AV Player") {
+            // Example 2: Same AVKit player, but customized using an asset with "AVURLAssetHTTPHeaderFieldsKey" and a custom User-Agent
+            // Apple's CFNetwork will be used under the hood to make the underlying HTTP calls using the custom User-Agent header
+            // However, "AVURLAssetHTTPHeaderFieldsKey" appears nowhere in Apple's public documentation, and can break at any time,
+            // also possibly grounds for an app rejection!
+            Button("AVPlayer · custom User-Agent · undocumented API · range ✅") {
+                let headers: [String : String] = ["User-Agent": myUserAgent]
+                let asset = AVURLAsset(url: URL(string: audioUrl)!, options: ["AVURLAssetHTTPHeaderFieldsKey": headers])
+                player.replaceCurrentItem(with: AVPlayerItem(asset: asset))
+                player.play() // "My Podcast App"
+            }.padding()
+            
+            // Example 2b: Same as example 2, but the media server does not support range requests
+            // Apple's CFNetwork will be used under the hood to make the first HTTP call using the custom User-Agent header
+            // (the first call is for the first two bytes) - however, the custom header is not used for the subsequent call
+            // (ie, the call for the entire file without a range, the one that gets played!)
+            Button("AVPlayer · custom User-Agent · undocumented API · no range ❌") {
+                let headers: [String : String] = ["User-Agent": myUserAgent]
+                let asset = AVURLAsset(url: URL(string: audioUrlNotSupportingRangeRequests)!, options: ["AVURLAssetHTTPHeaderFieldsKey": headers])
+                player.replaceCurrentItem(with: AVPlayerItem(asset: asset))
+                player.play() // "Apple Core Media"
+            }.padding()
+            
+            // Example 3: Same AVKit player, but customized using a resource loader delegate, a documented public API (AVAssetResourceLoaderDelegate)
+            // The custom delegate is responsible for providing data back to the player, so it can make the HTTP calls itself,
+            // which means it can easily set the User-Agent (using the standard URLRequest is this demo)
+            // This is much more difficult integration, since the player calls the delegate multiple times with different ranges etc
+            Button("AVPlayer · custom User-Agent · documented API · range ✅") {
                 // must not use "https" or "http", the custom loader delegate will not be called!
                 let asset = AVURLAsset(url: URL(string: "custom-\(audioUrl)")!)
                 asset.resourceLoader.setDelegate(customLoaderDelegate, queue: .global(qos: .default))
                 player.replaceCurrentItem(with: AVPlayerItem(asset: asset))
                 player.play() // "My Podcast App"
             }.padding()
+            
+            // Example 3b: Same as example 3, but the media server does not support range requests
+            // This still works since we are in complete control of the underlying HTTP calls
+            Button("AVPlayer · custom User-Agent · documented API · no range ✅") {
+                // must not use "https" or "http", the custom loader delegate will not be called!
+                let asset = AVURLAsset(url: URL(string: "custom-\(audioUrlNotSupportingRangeRequests)")!)
+                asset.resourceLoader.setDelegate(customLoaderDelegate, queue: .global(qos: .default))
+                player.replaceCurrentItem(with: AVPlayerItem(asset: asset))
+                player.play() // "My Podcast App"
+            }.padding()
         }
+        
     }
+    
 }
 
-// helper class to handle loading of a custom asset url
+// MARK: - Additional code to support Example 3 (custom AVAssetResourceLoaderDelegate)
+
+
+// Helper class to handle loading of a custom asset url.
+// This shows how the underlying http calls can be completely customized by the app at this point.
+// The code below should not be copy and pasted as is!  It's deliberately minimal to prove out the mechanism.
+// A real app should not pre-download and cache the entire file, otherwise there is no benefit to streaming.
+// It should turn around and make HTTP range requests, gracefully handle failures, etc.
+// It's done below to keep things short, and to show the complexity of this approach (a minimal example is already complicated!)
 class CustomLoaderDelegate : NSObject, AVAssetResourceLoaderDelegate {
     let userAgent: String
     var fetchedUrlData = [URL: Data]() // cache the response per url, since we get multiple callbacks for the same resource
@@ -37,6 +101,8 @@ class CustomLoaderDelegate : NSObject, AVAssetResourceLoaderDelegate {
     }
     
     func resourceLoader(_ resourceLoader: AVAssetResourceLoader, shouldWaitForLoadingOfRequestedResource loadingRequest: AVAssetResourceLoadingRequest) -> Bool {
+        // handle requests from the player, turn around and make an HTTP call ourself if we have not already fetched it
+        // then send the data back to the player
         if let url = loadingRequest.request.url, let dataRequest = loadingRequest.dataRequest {
             func finishRequestWithData(_ data: Data) {
                 if let contentInformationRequest = loadingRequest.contentInformationRequest {
@@ -71,13 +137,14 @@ class CustomLoaderDelegate : NSObject, AVAssetResourceLoaderDelegate {
     }
 }
 
-// helper to fetch an url with a custom user-agent
+// Helper function to fetch an url
+// This is deliberately simplistic, a real app should make multiple range requests, implement their own cache, etc.
 func fetch(url: URL, userAgent: String, onData: @escaping (Data) -> ()) {
     print("fetch \(url)")
     let session = URLSession.shared
     
     var urlRequest = URLRequest(url: url)
-    urlRequest.addValue(userAgent, forHTTPHeaderField: "User-Agent")
+    urlRequest.addValue(userAgent, forHTTPHeaderField: "User-Agent") // finally, setting the custom User-Agent!
     
     let task = session.dataTask(with: urlRequest) { (data, response, error) in
         guard error == nil else {
@@ -101,4 +168,5 @@ func fetch(url: URL, userAgent: String, onData: @escaping (Data) -> ()) {
     task.resume()
 }
 
+// run the playground
 PlaygroundPage.current.setLiveView(ContentView())
